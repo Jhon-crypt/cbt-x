1e9f211fed17b6bb96937145e6d10421983db37e
Jhon-crypt <johnoladele690@gmail.com>
2022-12-06 21:31:04 +0500

    Added favicon

--- index.php
+++ index.php
@@ -9,6 +9,14 @@
             <meta name="author" content="Oladele John, JohnOx">
             <title>CBT-X</title>
 
+            <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+
+            <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+
+            <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+
+            <link rel="manifest" href="site.webmanifest">
+
             <!-- Bootstrap core CSS -->
             <link href="./app/assets/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
