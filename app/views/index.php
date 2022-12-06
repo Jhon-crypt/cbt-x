cffcd268aa3ecf233edd78351acadc4328f556f9
Jhon-crypt <johnoladele690@gmail.com>
2022-12-06 21:43:19 +0500

    Just changed something

---
+++ app/views/index.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\views;
+
+class views{
+
+    public $index_page;
+
+    public function displayIndexPage(){
+
+        $this->index_page = '
+        
+        
+        
+        ';
+
+    }
+
+}
+
+?>
