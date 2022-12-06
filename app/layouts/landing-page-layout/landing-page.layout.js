52a4a4302b89da864854402c3ea2ea76fa5b4c33
Jhon-crypt <johnoladele690@gmail.com>
2022-12-07 03:09:15 +0500

    Changed something on the landing page layout

--- app/layouts/landing-page-layout/landing-page.layout.js
+++ app/layouts/landing-page-layout/landing-page.layout.js
@@ -5,15 +5,27 @@ app.directive("landingPage", function(){
         template : `
         
         <div class="px-4 py-5 my-5 text-center">
-            <img class="d-block mx-auto mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-            <h1 class="display-5 fw-bold">CBT-X</h1>
+            <h1 class="display-5 fw-bold"><font color="#00AAF4"><i class="fa fa-check-square-o"></i> CBT</font>-<font color="orange">X</font></h1>
             <div class="col-lg-6 mx-auto">
-                <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-                <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                    <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
-                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
-                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
-                </div>
+                <p class="lead mb-4 text-center">
+                    Cbt-X is designed to give students and others a platform to adequately prepare and simulates the real exam/test situation, 
+                    Tests/exams can be conducted simultaneously for multiple candidates irrespective of their locations and also auto-grading
+                    easily to save time in evaluating large answers.
+                </p>
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link text-light">
+                            <button class="btn btn-lg text-light" style="background:#00AAF4"> Sign up <i class="fa fa-user-circle-o"></i> </button>
+                        </a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link text-light">
+                            <button class="btn btn-lg text-light" style="background:#00AAF4"> Practise test/exam <i class="fa fa-check-square-o"></i> </button>
+                        </a>
+                    </li>
+
+                </ul>
             </div>
         </div>
         
