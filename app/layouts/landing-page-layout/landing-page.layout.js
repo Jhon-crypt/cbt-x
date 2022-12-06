9190a2b7e80f20abac7c26ac4f021ef1386fbabf
Jhon-crypt <johnoladele690@gmail.com>
2022-12-07 02:13:13 +0500

    Added the landing page branch

---
+++ app/layouts/landing-page-layout/landing-page.layout.js
@@ -0,0 +1,13 @@
+app.directive("landingPage", function(){
+
+    return {
+
+        template : `
+        
+        <h1>Hello Cbt-X</h1>
+        
+        `
+
+    }
+
+});
---
+++ app/route/route.js
@@ -0,0 +1,8 @@
+app.config(function($routeProvider){
+
+    $routeProvider
+    .when("/", {
+        templateUrl : "./app/views/landing-page/landing-page.view.html"
+    })
+
+});
