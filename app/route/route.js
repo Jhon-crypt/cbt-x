79e872104fbb05fde93724852a105f48cbd6438e
Jhon-crypt <johnoladele690@gmail.com>
2022-12-07 20:45:15 +0500

    Uodated the route

--- app/route/route.js
+++ app/route/route.js
@@ -5,7 +5,19 @@ $routeProvider
         templateUrl : "./app/views/searchView/search.view.html"
     })
     .when("/signup", {
+        templateUrl : "./app/views/signupView/signup.view.html"
+    })
+    .when("/login", {
         templateUrl : "./app/views/loginView/login.view.html"
+    })
+    .when("/success", {
+        templateUrl : "./app/views/signupView/success.view.html"
+    })
+    .when("/dashboard", {
+        templateUrl : "./app/views/dashboardView/dashboard.view.html"
+    })
+    .when("/create", {
+        templateUrl : "./app/views/createView/create.view.html"
     })
 
 });
