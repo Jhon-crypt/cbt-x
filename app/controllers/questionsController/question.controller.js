a1c76ab04ccb750156799318d8a937edce00303a
Jhon-crypt <johnoladele690@gmail.com>
2022-12-09 19:56:12 +0500

    Added controllers

---
+++ app/controllers/questionsController/question.controller.js
@@ -0,0 +1,23 @@
+app.controller("questionsController", 
+
+    function($scope){
+
+        $scope.question = [
+
+            {
+                title:'Which is not a front end framework',
+                optionA:'Php',optionB:'AngularJs',optionC:'React',
+                optionD:'I dont know',number:'1'
+            },
+            {
+                title:'Which is not a front end framework',
+                optionA:'Php',optionB:'AngularJs',optionC:'React',
+                optionD:'I dont know',number:'1'
+            },
+
+
+        ];
+
+    }
+
+);
--- app/controllers/test-exams-cardsController/test-exams.controller.js
+++ app/controllers/test-exams-cardsController/test-exams.controller.js
@@ -8,10 +8,6 @@ app.controller("testAndExamsController",
                 title:'Simple Test',about:'This test is sooo simple even for a 1 year old',question:'20',
                 timeLimit:'15',type:'test',status:'active',dateCreated:'7/12/2022',timeCreated:'03:07',author:'coding_wizard'
             },
-            {
-                title:'Alien Test',about:'This test is only for aliens from space',question:'700',
-                timeLimit:'2',type:'exam',status:'active',dateCreated:'7/12/2029',timeCreated:'03:07',author:'star_wars'
-            },
 
         ];
 
