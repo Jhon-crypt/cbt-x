651c34b53228658a82f77e5d781838eb2170514d
Jhon-crypt <johnoladele690@gmail.com>
2022-12-10 04:29:40 +0500

    Added participated users controller

---
+++ app/controllers/participatedUsersController/participated-users.controller.js
@@ -0,0 +1,19 @@
+app.controller("usersParticipated", 
+
+    function($scope){
+
+        $scope.users = [
+
+            {
+                username:'Coding_wizard',score:'20'
+            },
+            {
+                username:'Coding_wizard',score:'20'
+            },
+
+        ]
+
+
+    }
+
+)
