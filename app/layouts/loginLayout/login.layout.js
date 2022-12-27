1787e70f91d5657a845493b682cadf91bdc56dcd
Jhon-crypt <johnoladele690@gmail.com>
2022-12-27 23:47:02 +0500

    Updated layouts

--- app/layouts/loginLayout/login.layout.js
+++ app/layouts/loginLayout/login.layout.js
@@ -6,32 +6,49 @@ app.directive("login", function(){
         
         <div class="container pt-5 mt-5" align="center">
 
-            <form>
+            <form ng-controller="login" ng-submit="login()">
                 <h3><font color="#00AAF4"><i class="fa fa-check-square-o"></i> CBT</font>-<font color="orange">X</font></h3>
                 <h2 class="h3 mb-3 fw-normal">Login</h2>
+                
+                <div ng-repeat="x in loginStatus">
+                    <div class="alert alert-success" style="width:300px" 
+                    ng-show="{{ x.loggedIn }}">
+                        <h4 class="mb-3">Logged In </h4>
+                        <a href="">
+                            <button class="btn btn-lg text-light" 
+                            style="background-color:#00AAF4;">
+                                <small>Click Here To Continue <i class="fa fa-external-link"></i></small>
+                            </button>
+                        </a>
+                    </div>
+
+                    <div class="alert alert-warning" style="width:300px" 
+                    ng-show="{{ x.loginError }}">
+                        Wrong Email Or Password
+                    </div>
+                    
+                </div>
     
                 <div class="form-floating mb-3" style="width:300px">
-                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                    <input type="email" class="form-control" ng-model="user.email" id="floatingInput" placeholder="name@example.com" required>
                     <label for="floatingInput"><i class="fa fa-envelope"></i> Email</label>
                 </div>
 
                 <div class="form-floating mb-3" style="width:300px">
-                    <input type="password" class="form-control" id="floatingInput" placeholder="coding_wizard">
+                    <input type="password" class="form-control" ng-model="user.password" id="floatingInput" placeholder="coding_wizard" required>
                     <label for="floatingInput"><i class="fa fa-lock"></i> Password</label>
                 </div>
 
                 <small>
                     <p>Forgot Password, <a href="#!forgotPassword">Click here</a></p>
                 </small>
-        
-                <a href="#!dashboard" style="text-decoration:none;">
-                <button class="btn btn-lg btn-primary" type="submit" style="width:300px;background-color:#00AAF4;">
-                    <b>Login <i class="fa fa-sign-in"></i></b>
-                </button>
-                </a>
+
+                <input style="width:300px;background-color:#00AAF4;" 
+                type="submit" name="loginNow" class="btn btn-lg btn-primary" 
+                ng-disabled="login.$invalid" value="Login Now"/>
 
                 <p>Don't have an account, Sign Up <a href="#!signup">here</a></p>
-                
+
             </form>            
 
         </div>
--- app/layouts/signupLayout/signup.layout.js
+++ app/layouts/signupLayout/signup.layout.js
@@ -9,15 +9,16 @@ app.directive("signup", function(){
             <h3><font color="#00AAF4"><i class="fa fa-check-square-o"></i> CBT</font>-<font color="orange">X</font></h3>
                 <h2 class="h3 mb-3 fw-normal">Sign Up</h2>
 
-            <form action="app/services/signupService/signup.service.php" method="POST">
+            <form ng-controller="signup" ng-submit="signup()">
     
                 <div class="form-floating mb-3" style="width:300px">
-                    <input  type="email" class="form-control" id="floatingInput" placeholder="name@example.com" required>
+                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" required 
+                    ng-model="user.email">
                     <label for="floatingInput"><i class="fa fa-envelope"></i> Email</label>
                 </div>
 
                 <div class="form-floating mb-3" style="width:300px">
-                    <select class="form-select" >
+                    <select class="form-select" ng-model="user.gender">
                         <option>Gender</option>
                         <option>Male</option>
                         <option>Female</option>
@@ -25,21 +26,29 @@ app.directive("signup", function(){
                 </div>
 
                 <div class="form-floating mb-3" style="width:300px">
-                    <input type="text" class="form-control" id="floatingInput" placeholder="coding_wizard" required>
+                    <input type="text" class="form-control" id="floatingInput" placeholder="coding_wizard" required 
+                    ng-model="user.username">
                     <label for="floatingInput"><i class="fa fa-user-circle-o"></i> Username</label>
                 </div>
 
                 <div class="form-floating mb-3" style="width:300px">
-                    <input type="password" class="form-control" id="floatingInput" placeholder="coding_w" required>
+                    <input type="password" class="form-control" id="floatingInput" placeholder="coding_w" required 
+                    ng-model="user.password">
                     <label for="floatingInput"><i class="fa fa-lock"></i> Password</label>
                 </div>
 
                 <p>Already have an account, Login <a href="#!login">here</a></p>
                 
-                <button class="btn btn-lg btn-primary" type="submit" style="width:300px;background-color:#00AAF4;">
-                    <b>Sign Up <i class="fa fa-user-circle"></i></b>
-                </button>
+                <input style="width:300px;background-color:#00AAF4;" 
+                type="submit" name="signUp" class="btn btn-lg btn-primary" 
+                ng-disabled="signup.$invalid" value="Sign Up Now"/>
                 
+                <!--
+                <div ng-repeat="x in myData">
+                    <p>{{ x.email }}
+                </div>
+                -->
+
             </form>            
 
         </div>
