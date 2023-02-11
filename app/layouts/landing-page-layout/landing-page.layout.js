8c0f63d477ec0f976eb642ba405011a37788a3c3
Jhon-crypt <johnoladele690@gmail.com>
2023-02-11 23:57:07 +0500

    Made some changes

--- app/layouts/landing-page-layout/landing-page.layout.js
+++ app/layouts/landing-page-layout/landing-page.layout.js
@@ -8,20 +8,13 @@ app.directive("landingPage", function(){
             <h1 class="display-5 fw-bold"><font color="#00AAF4"><i class="fa fa-check-square-o"></i> CBT</font>-<font color="orange">X</font></h1>
             <div class="col-lg-6 mx-auto">
                 <p class="lead mb-4 text-center">
-                    Cbt-X is designed to give students and others a platform to adequately prepare and simulates the real exam/test situation, 
-                    Tests/exams can be conducted simultaneously for multiple candidates irrespective of their locations and also auto-grading
-                    easily to save time in evaluating large answers.
+                    Cbt-X is simply a platform that simulates the real exam/test situation, Create tests, practice and analyse on the go.
                 </p>
                 <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <a class="nav-link text-light">
-                            <button class="btn btn-lg text-light" style="background:#00AAF4"> Sign up <i class="fa fa-user-circle-o"></i> </button>
-                        </a>
-                    </li>
 
                     <li class="nav-item">
                         <a class="nav-link text-light">
-                            <button class="btn btn-lg text-light" style="background:#00AAF4"> Practise test/exam <i class="fa fa-check-square-o"></i> </button>
+                            <button class="btn btn-lg text-light" style="background:#00AAF4"> Get Started <i class="fa fa-external-link"></i> </button>
                         </a>
                     </li>
 
@@ -30,7 +23,7 @@ app.directive("landingPage", function(){
             </div>
 
             <div class="text-center pt-5 text-muted">
-                <small>Made with &#128151 by &#x26A1 Oladele John and STA © 2022</small>
+                <small>Made with &#128151 by &#x26A1 Oladele John © 2022</small>
             </div>
 
         </div>
